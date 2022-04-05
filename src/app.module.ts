3a9761dfd1909857f5edb64031f6905f18d02270
pjn141414 <jina071049@gmail.com>
2022-04-06 01:23:11 +0900

    Add: module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './apis/auth/auth.module';
+import { UploadController } from './apis/upload/upload.controller';
+import { UploadService } from './apis/upload/upload.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { config } from './config/ormConfig';
+import CatchException from './lib/error/catchException';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(config), AuthModule, ConfigModule],
+  controllers: [AppController, UploadController],
+  providers: [AppService, {
+    provide: APP_FILTER,
+    useClass: CatchException,
+  }, UploadService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // multer
+  const app: NestExpressApplication = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
+  app.use('/public', express.static(join(__dirname, '../public')));
+
+  await app.listen(8000);
 }
+
 bootstrap();

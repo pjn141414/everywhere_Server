791b33da17b923df1391b270a3f57370e284befa
pjn141414 <jina071049@gmail.com>
2022-04-06 01:22:14 +0900

    Add: base response

---
+++ src/lib/response/base.response.ts
@@ -0,0 +1,15 @@
+export default class BaseReponse<T> {
+  status: number;
+  message: string;
+  data?: T;
+
+  constructor(
+    status: number,
+    message: string,
+    data?: T
+  ) {
+    this.status = status;
+    this.message = message;
+    this.data = data;
+  }
+}
